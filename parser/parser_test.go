b302be6bc79261a5d313775dad79121eddc99af4
rbender <rbender@basis.cloud>
2024-05-15 12:38:55 +0200

    got to inbook p44 the chapter 2.5
    also after havig the test broken like in the book
    have the broken test fixed.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -15,6 +15,7 @@ func TestLetStatement(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgramm()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgramm() returned nil")
 	}
@@ -54,4 +55,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errrors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
