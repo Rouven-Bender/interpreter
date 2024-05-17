e4499034cfc542da379e630909a4650feff50ff6
rbender <rbender@basis.cloud>
2024-05-17 11:31:40 +0200

    got the tests for prefix expressions
    inbook p61 after the test

--- parser/parser.go
+++ parser/parser.go
@@ -59,6 +59,7 @@ func (p *Parser) Errors() []string { return p.errors }
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 	p.prefixParseFns[tokenType] = fn
 }
+
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
 }
