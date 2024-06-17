d0c6b5dfada8cb4753729b86150c8d3155b68600
rbender <rbender@basis.cloud>
2024-06-17 10:33:26 +0200

    Got the String eval done
    string concat next

--- object/object.go
+++ object/object.go
@@ -14,6 +14,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ       = "STRING"
 )
 
 type ObjectType string
@@ -105,3 +106,10 @@ func NewEnclosedEnvironmnet(outer *Environment) *Environment {
 	env.outer = outer
 	return env
 }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
