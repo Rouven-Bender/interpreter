5adfa25ee16d9c04f50ae1af093b5b6de7d2e075
rbender <rbender@basis.cloud>
2024-06-24 14:59:25 +0200

    added the support for len on arrays a

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,12 +3,14 @@ package evaluator
 import "inter/object"
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
+	"len": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
 			}
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
